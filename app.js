73c7efc16d575567f87d6428d3a50349d2ab9369
Levon <levon.gambaryan@gmail.com>
2019-05-14 22:08:36 +0300

    CORS add

--- app.js
+++ app.js
@@ -36,6 +36,12 @@ const values = Object.values(ALBUMS);
 // Static
 app.use('/images', express.static(__dirname + '/images'));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/albums', (req, res) => {
   res.json(keys);
 });
